2992458c4a992c50ece6246fc17d85e4c34d00d9
Eyouel Kibret <eyouel2016@gmail.com>
2025-10-29 23:51:00 -0400

    first commit

---
+++ contains_duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = defaultdict()
+
+        for num in nums:
+            if num in seen:
+                return True
+            else:
+                seen[num] = 1
+        return False
