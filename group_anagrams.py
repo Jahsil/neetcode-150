e27afe59ed9c102faddba6b7d784fd071ca17df8
Eyouel Kibret <eyouel2016@gmail.com>
2025-11-01 15:05:04 -0400

    add group anagrams

---
+++ group_anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        N = len(strs)
+        hashmap = defaultdict(list)
+
+        for s in strs:
+            hashmap["".join(sorted(s))].append(s)
+
+        return list(hashmap.values())
